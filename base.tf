56d7e5e616797e3dcf73f99c67e01752706648e0
BharathiMeda <128385999+BharathiMeda@users.noreply.github.com>
2025-05-05 10:37:21 +0530

    Update base.tf

--- base.tf
+++ base.tf
@@ -1,21 +1,10 @@
-# Configure the Terraform backend
 terraform {
-  /*backend "s3" {
-    bucket         = "your-terraform-state-bucket" # Replace with your S3 bucket name
-    key            = "vmware/terraform-vm/terraform.tfstate" # Path to the state file in the bucket
-    region         = "us-east-1" # Replace with your AWS region
-    encrypt        = true
-    dynamodb_table = "terraform-lock-table" # Optional: Table for state locking
-  }*/
-
+  required_version = ">= 0.13.4"
   required_providers {
     vsphere = {
-      source  = "hashicorp/vsphere"
-      version = "~> 2.0"
+      source = "hashicorp/vsphere"
     }
   }
-
-  required_version = ">= 1.0.0"
 }
 
 # Provider block for vSphere
